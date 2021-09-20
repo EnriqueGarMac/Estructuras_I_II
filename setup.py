416be43dc99ae09f79096385a075420e4b179849
Enrique García Macías <enriquegm@go.ugr.es>
2021-09-20 19:44:21 +0200

    Version_0

---
+++ setup.py
@@ -0,0 +1,24 @@
+import sys
+from setuptools import setup
+
+setup(
+    name="anastruct",
+    description="Análisis de pórticos planos",
+    author="Enrique GM",
+    author_email="enriquegm@ugr.es",
+    url="https://github.com/EnriqueGarMac/Estructuras_I",
+    license="GPL-3.0",
+    packages=[
+        "anastruct",
+        "anastruct.fem",
+        "anastruct.fem.system_components",
+        "anastruct.fem.examples",
+        "anastruct.material",
+        "anastruct.cython",
+        "anastruct.fem.cython",
+        "anastruct.fem.plotter",
+        "anastruct.fem.util",
+        "anastruct.sectionbase",
+    ],
+    install_requires=["matplotlib>=3.0", "numpy>=1.15.4", "scipy>=1.1.0"],
+)
